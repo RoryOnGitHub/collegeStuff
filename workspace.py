024974cf67d8989a3bad12a6e58adf00a6c2f825
Rory <rory.murphy@landregistry.gov.uk>
2023-08-17 13:54:14 +0100

    added bitcoin buy or sell tracker

--- workspace.py
+++ workspace.py
@@ -1,3 +1,33 @@
-# test
-# comment for new commit
+import requests
+import json
+from win10toast import ToastNotifier
+import time
+
+drops = 0
+rises = 0
+previous_price = ""
+while True:
+    t = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=gbp").text
+    x = json.loads(t)
+
+    price = str(x["bitcoin"]["gbp"])
+    print(price)
+    toast = ToastNotifier()
+
+    if price < previous_price:
+        drops += 1
+        rises = 0
+    elif price > previous_price:
+        rises += 1
+        drops = 0
+
+    if drops >= 3:
+        message = "Quick panic sell and make a loss! It was {} and now:".format(previous_price)
+        toast.show_toast(message, price, duration=5)
+    elif rises >= 3:
+        message = "Buy, buy, buy while its more expensive! It was {} and now:".format(previous_price)
+        toast.show_toast(message, price, duration=5)
+    time.sleep(5)
+    previous_price = price
+
 
