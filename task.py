d189afa7628fd9ad9accdf28e67322f6b9bf836b
Rory <rory.murphy@landregistry.gov.uk>
2023-08-17 14:52:17 +0100

    improvements on phone number formatter

--- task.py
+++ task.py
@@ -1,23 +1,14 @@
 import phonenumbers
 
-# old_string_of_numbers = "(694)544-2366, (420)  647-3944, ( 321) 1526278, (776) 949-8387, 351588-8871"
-# with open("old_numbers.txt", "w") as handler:
-#     handler.write(old_string_of_numbers)
-
 with open("old_numbers.txt", "r") as handler:
-    lines = handler.read()
+    lines = handler.read().split(", ")
 
-numbers = lines.split(", ")
-# print(a)
-# numbers = ["(694)544-2366", "(420)  647-3944", "( 321) 1526278", "(776) 949-8387", "351588-8871"]
 new_number = []
-for i in numbers:
+for i in lines:
     new_number.append(phonenumbers.format_number(phonenumbers.parse(i, 'US'), phonenumbers.PhoneNumberFormat.NATIONAL))
 
-new_string_of_numbers = ", ".join(new_number)
-
 with open("new_numbers.txt", "w") as handler:
-    handler.write(new_string_of_numbers)
+    handler.write(", ".join(new_number))
 
 
 
